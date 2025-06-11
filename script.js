0763b5a5d6fc5260dbf41124d92aa5549e0b6d91
alumno <alumno@Instituto.local>
2025-06-11 08:23:11 -0300

    repeticiones

---
+++ script.js
@@ -0,0 +1,32 @@
+//1
+
+let animales = ["perro", "gato", "conejo", "loro"];
+for (let indice = 0; indice < animales.length; indice++) {
+    console.log(animales[indice])
+    
+}
+
+//2
+
+for (let indice = 0; indice < animales.length; indice++) {
+    if (indice % 2=== 0)
+        console.log(animales[indice])}
+
+//3
+
+let numeros = [4, 7, 2, 9, 5];
+suma= 0
+
+for (let i = 0; i < numeros.length; i++) {
+    suma += numeros[i]
+    console.log(suma);
+    
+}
+
+//4
+
+let datos = [3, 6, 1, 8];
+for (let i = 0; i < datos.length; i++) {
+    console.log(datos[i]);
+    
+}
