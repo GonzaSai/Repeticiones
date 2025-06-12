9963c08d3cf26c5fae566d6bdf695de6e5b52bf9
alumno <alumno@Instituto.local>
2025-06-12 11:24:26 -0300

    Repeticiones

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ for (let indice = 0; indice < animales.length; indice++) {
 //3
 
 let numeros = [4, 7, 2, 9, 5];
-suma= 0
+let suma= 0
 
 for (let i = 0; i < numeros.length; i++) {
     suma += numeros[i]
@@ -26,7 +26,40 @@ for (let i = 0; i < numeros.length; i++) {
 //4
 
 let datos = [3, 6, 1, 8];
-for (let i = 0; i < datos.length; i++) {
-    console.log(datos[i]);
+let resultado = [];
+
+for (let i = 0; i < datos.length ; i++) {
+    resultado.push(datos[i]*2)
+    console.log(resultado)
     
 }
+
+//5
+
+let alumnos = ["Sofía", "Carlos", "Bruno", "Lucía", "Carla"];
+let introduceNombre = prompt('Introduce un nombre')
+if (alumnos.includes(introduceNombre)) {
+    console.log('Se ha encontrado el nombre correctamente')
+} else {
+    console.log('No se ha encontrado el')
+}
+
+//6
+
+let valores = [5, 0, 8, 0, 2];
+for (let i = 0; i < valores.length; i++) {
+    if (valores[i] === 0) {
+        valores[i] = 100;
+    }
+}
+console.log(valores);
+
+//10
+
+let notas = [8, 7, 9, 10, 6];
+let division= 1
+for (let i = 0; i < notas.length; i++) {
+    
+    
+}
+
